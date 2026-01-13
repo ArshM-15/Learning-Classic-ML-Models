f542fc92da12b6d9f4e14ae64c6fd9401e9de0ef
Arshvir Mundi <arshvir.mundi@gmail.com>
2026-01-13 14:14:28 -0500

    finished regression models

--- regression/decision_tree_regression/decision_tree_regression.py
+++ regression/decision_tree_regression/decision_tree_regression.py
@@ -1,6 +1,8 @@
 # Decision Tree Regression
 
 # Importing the libraries
+from sklearn.tree import export_text
+from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,19 +13,22 @@ X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Training the Decision Tree Regression model on the whole dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
+regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
 
 # Predicting a new result
-regressor.predict([[6.5]])
+print(regressor.predict([[7.6]]))
 
 # Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = np.arange(X.min(), X.max(), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
 plt.title('Truth or Bluff (Decision Tree Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
+# plt.show()
+
+# Print the tree structure as text
+tree_rules = export_text(regressor, feature_names=['Position Level'])
+print(tree_rules)
